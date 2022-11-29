dbc951832950e3e68bb58209657b64278fe73801
MichalOle <molesinski1994@gmail.com>
2022-11-29 12:47:46 +0100

    Debuging lesson

---
+++ intro/src/main/java/LeapYear.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println("Podaj rok. Sprawdzimy czy to rok przestępny");
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+
+        int score = year%4;
+
+        if(score == 0){
+
+            System.out.println("Podany rok  jest rokiem przestępnym");
+        }
+        else {
+            System.out.println("Rok  nie jest rokiem przestępnym");
+        }
+
+    }
+}
